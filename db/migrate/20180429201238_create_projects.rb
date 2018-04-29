6a23dad7a834dd1e46adb5476ff91ed708a4c93e
Dan Kiss <flowfast47@gmail.com>
2018-04-29 13:27:45 -0700

    add devise setup

--- db/migrate/20180429201238_create_projects.rb
+++ db/migrate/20180429201238_create_projects.rb
@@ -1,6 +1,6 @@
 class CreateProjects < ActiveRecord::Migration[5.2]
   def change
-    create_table :projects do |t|
+    create_table :products do |t|
       t.column :price, :decimal
       t.column :name, :string
     end
