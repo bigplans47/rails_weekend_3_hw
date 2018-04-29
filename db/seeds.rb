fb49cfd4acc62072d97ddf6e3fb015608aa98c1f
Dan Kiss <flowfast47@gmail.com>
2018-04-29 15:49:10 -0700

    add product seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create_table :products do |t|
+#   t.column :price, :decimal
+#   t.column :name, :string
+#
+#   create_table :order_items do |t|
+#     t.column :quanity, :integer
+#     t.column :product_id, :bigint
+#     t.column :order_id, :bigint
+
+10.times do |i|
+  Product.create!(:name => Faker::Coffee.blend_name, :price => Faker::Number.decimal(2))
+  # @product_id = Product.last.id
+  # 3.times do |g|
+  #   OrderItem.create!(:quanity => Faker::Number.between(1,15), :product_id => @product_id, :order_id => 1)
+  # end
+end
